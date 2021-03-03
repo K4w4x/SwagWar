879bb1cf82f118784cd6182e7ccd598d95f0660b
Kawax <k4w4x145@gmail.com>
2021-03-03 06:55:58 +0100

    Changes on Redcode constructor & MARS Reader 1st iteration

--- src/simulation/Test.java
+++ src/simulation/Test.java
@@ -5,6 +5,8 @@ public class Test {
     public static void main(String[] args) {
         Mars m = new Mars();
         System.out.println(m.toString());    
+        
+        System.out.println(s[0]);
     }
 
 }
